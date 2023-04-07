759762aca54ec8878daa68e9fa443ca766406a02
Alexandre Douglas <xand.douglass2@gmail.com>
2023-04-07 08:14:13 -0300

    Update BadgeUser

--- users/admin.py
+++ users/admin.py
@@ -2,17 +2,24 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import BadgeUser
 
 CustomUser = get_user_model()
+
+
+class BadgeUserAdmin(admin.ModelAdmin):
+    list_display = ("id","title",)
+
+admin.site.register(BadgeUser, BadgeUserAdmin)
 
 class CustomUserAdmin(UserAdmin):
 	add_form = CustomUserCreationForm
 	form = CustomUserChangeForm
 	model = CustomUser
-	list_display = ['email', 'username']
+	list_display = ['email', 'username', 'badge']
 	fieldsets = (
 		(None, {'fields': ('email', 'password')}),
-		('Informações Pessoais', {'fields':('first_name', 'last_name', 'bio', 'birth_date', 'perfilIMG','badgeTITLE','badgeIMG')}),
+		('Informações Pessoais', {'fields':('first_name', 'last_name', 'bio', 'birth_date', 'perfilIMG','badge')}),
 		('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
 		('Datas importantes', {'fields': ('last_login', 'date_joined')})
 	)
--- users/models.py
+++ users/models.py
@@ -1,11 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class BadgeUser(models.Model):
+    title = models.CharField('Títlo da Badge', max_length=40)
+    badgeIMG = models.ImageField('Badge',upload_to='badge/', blank=True)
+
+    def __str__(self):
+        return self.title
 
 
 class CustomUser(AbstractUser):
     bio = models.TextField('Biografia',max_length=500, blank=True)
     birth_date = models.DateField('Data de aniversário',null=True, blank=True)
     perfilIMG = models.ImageField('Imagem do perfil',upload_to='perfil/', blank=True)
-    badgeTITLE = models.CharField('Título do emblema', max_length=50, blank=True, default='Novo aqui :)')
-    badgeIMG = models.ImageField('Badge',upload_to='badge/', blank=True)
+    badge = models.ForeignKey(BadgeUser,on_delete=models.CASCADE, related_name='badge', default=1)
